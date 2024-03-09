54cc2200d5cf5644829f96ae182d3b196831a22e
VeronikaProvorna <74527896+VeronikaProvorna@users.noreply.github.com>
2024-03-09 01:04:17 +0100

    commit in branch1

--- rgr2.js
+++ rgr2.js
@@ -4,12 +4,12 @@ const closeBtn = document.getElementById("closeBtn");
 closeBtn.addEventListener("dblclick", closeBanner);
 
 //змінимо час показу банера - зміна в branch 1
-setTimeout(showBanner, 5000);
+setTimeout(showBanner, 4000);
 
 function closeBanner(){
     banner.style.display = "none";
     //змінимо час показу банера - зміна в branch 1
-    setTimeout(showBanner, 5000);
+    setTimeout(showBanner, 4000);
 }
 
 function showBanner(){
